8f931d4fcbbf2e2cef10d139e145715f0458df09
Egil Johnsen Dahle <egil_dahle@hotmail.com>
2026-01-29 09:44:21 +0100

    Create clean-pages.js

---
+++ script/clean-pages.js
@@ -0,0 +1,28 @@
+function cleanMain() {
+    const main = document.querySelector("main");
+    if (!main)
+        return;
+    main.removeAttribute("id");
+    main.innerHTML = "";
+}
+function cleanHeader() {
+    const header = document.querySelector("header");
+    if (!header)
+        return;
+    header.removeAttribute("id");
+    header.innerHTML = "";
+}
+function cleanAside() {
+    const aside = document.querySelector("aside");
+    if (!aside)
+        return;
+    aside.removeAttribute("id");
+    aside.innerHTML = "";
+}
+function cleanFooter() {
+    const footer = document.querySelector("footer");
+    if (!footer)
+        return;
+    footer.removeAttribute("id");
+    footer.innerHTML = "";
+}
